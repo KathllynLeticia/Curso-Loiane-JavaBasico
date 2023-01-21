928c5c3cd8a349fdd633e6c5a1865da470e4e62d
KathllynLeticia <kathllyn.leticia@gmail.com>
2023-01-21 19:03:16 -0300

    Aulas Java

---
+++ Aula 05/HelloWorld.java
@@ -0,0 +1,17 @@
+/*Class Declaração da CLasse
+*HelloWorld Nome Da Classe 
+*/
+class HelloWorld{
+
+/*Método 
+* Public static - (Modificadores de acesso do Método)
+* Void - Tipo de Retorno do Método
+* Main - Nome do Método
+*(String [] args) - Parâmetros/Argumentos do Método
+* System.out.println - Nosso Código
+*{} São Blocos 
+*/
+	public static void main (String [] args){ 
+		System.out.println("Hello World!");
+	}
+}
---
+++ Aula 05/OláMundo.java
@@ -0,0 +1,5 @@
+class OláMundo{
+	public static void main (String [] args){
+	System.out.println ("Olá Mundo!");
+	}
+}
